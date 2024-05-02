ce81a4bccb40f4cd0729736c9231b806f13abc0e
sonya-sh <sonya.shh@mail.ru>
2024-05-03 00:15:25 +0300

    добавлен тестовый .env.2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+
+env_path = '.env.2'
+load_dotenv(env_path)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
