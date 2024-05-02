d2da0a17cb54ab6447705f1d40340ed195d3a23b
sonya-sh <sonya.shh@mail.ru>
2024-05-02 23:40:25 +0300

    исправлен контейнер nginx для staticfiles

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=True))
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS")
-
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'www.sonyasheblova.pythonanywhere.com', 'sonyasheblova.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:1337'] 
 
 # Application definition
 
@@ -141,13 +141,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'img')
 
